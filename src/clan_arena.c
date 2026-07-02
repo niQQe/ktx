7d46e4a04369fc047cb166baa2674a9499eb3a60
sopparus <martin.larsson2@gmail.com>
2026-07-02 10:57:30 +0200

    Revert "WIPEOUT: pre-round fire permission"
    This reverts commit 0f90f138b7488d09fed901d5c611753fa1900b5e.

--- src/clan_arena.c
+++ src/clan_arena.c
@@ -704,7 +704,7 @@ qbool CA_can_fire(gedict_t *p)
 		return true;
 	}
 
-	if (!match_in_progress && !ra_match_fight && p->ready)
+	if (!ra_match_fight && p->ready)
 	{
 		return true;	// allow fire during prewar if /ready
 	}
